ecde459e41e75b05c8fc5a1fcdffbca0644a1c76
Apoorva0915 <apoorvakesarwani001@gmail.com>
2025-02-21 20:35:37 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use("/uploads", express.static("uploads")); // Serve uploaded files
 app.use("/api/payment", paymentRoutes);
