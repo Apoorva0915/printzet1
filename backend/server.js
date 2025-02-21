67d0ab97a50fd6865298ec6cad9496833a51cd64
Apoorva0915 <apoorvakesarwani001@gmail.com>
2025-02-21 20:51:57 +0530

    make some changes on vercel.json

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads")); // Serve uploaded files
 app.use("/api/payment", paymentRoutes);
